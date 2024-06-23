cd2adb759be7f8e34437587471fabf838e026d07
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-06-23 22:07:39 +0530

    feat: verifyToken method logic updated

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -5,19 +5,18 @@ const generateToken = async (userPayload) => {
   return jwt.sign(userPayload, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
-const jwtAuthMiddleware = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   try {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-      return res.status(401).json({ message: "Token not found" });
+    const token = req.cookies.tokenCookie;
+    if (!token) {
+      return res.status(403).json({ success: false, message: "Unauthorized" });
     }
-    const token = authorization.split(" ")[1];
-    if (!token) return res.status(403).json({ message: "Unauthorized" });
-    req.userPayload = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.id = decoded.id;
     next();
   } catch (error) {
     res.status(403).json({ seccess: false, message: "Invalid Token" });
   }
 };
 
-module.exports = { generateToken, jwtAuthMiddleware };
+module.exports = { generateToken, verifyToken };
