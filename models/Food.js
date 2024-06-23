60fabf2b3d316c8e31c3c9113d3c788f24fb6995
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-06-23 22:06:26 +0530

    feat: tracks cart items

--- models/Food.js
+++ models/Food.js
@@ -1,6 +1,36 @@
 const mongoose = require("mongoose");
 
-const foodSchema = new mongoose.Schema({});
+const foodSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    totalPrice: {
+      type: Number,
+    },
+    quantity: {
+      type: Number,
+    },
+    rating: {
+      type: Number,
+    },
+    image: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 const Food = mongoose.model("food", foodSchema);
 module.exports = Food;
